5373748f5e658cd29d316b7ec2b12bea46d590a5
mahfuz <mahfuz.ullah@mislbd.com>
2024-11-15 22:56:57 +0600

    update for testing in spring boot 3.3.5

--- src/main/java/dev/mahfuj/kafka_started/config/KafkaStreamsConfig.java
+++ src/main/java/dev/mahfuj/kafka_started/config/KafkaStreamsConfig.java
@@ -1,13 +1,17 @@
 package dev.mahfuj.kafka_started.config;
 
-import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
+import static org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG;
 
 @Configuration
 @EnableKafkaStreams
@@ -19,11 +23,11 @@ public class KafkaStreamsConfig {
     @Value(value = "${spring.application.name}")
     private String moduleName;
 
-    @Bean(name = "defaultKafkaStreamsConfig")
-    public StreamsConfig kafkaStreamsConfig() {
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    KafkaStreamsConfiguration kStreamsConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, moduleName.concat("-streams"));
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
-        return new StreamsConfig(props);
+        props.put(APPLICATION_ID_CONFIG, moduleName.concat("-streams"));
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        return new KafkaStreamsConfiguration(props);
     }
 }
--- src/main/java/dev/mahfuj/kafka_started/streams/TaskProcessor.java
+++ src/main/java/dev/mahfuj/kafka_started/streams/TaskProcessor.java
@@ -2,8 +2,8 @@ package dev.mahfuj.kafka_started.streams;
 
 import dev.mahfuj.kafka_started.domain.AsyncProcessTask;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Autowired;
